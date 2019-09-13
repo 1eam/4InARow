02dcef4fc8fef64958e13508a6f73745dd14c2ca
Esther <esther191299@gmail.com>
2019-09-13 19:45:23 +0200

    Player class updated

--- src/main/java/com/novi/minigames/Player.java
+++ src/main/java/com/novi/minigames/Player.java
@@ -1,29 +1,36 @@
 package com.novi.minigames;
 
+
 public class Player {
-	private String name;
-	private char token;
-	private int score;
-	
-	public Player (String name, char token) {
-		this.name = name;
-		this.token = token;
-	}
-	
-	
-	
-	
-	public String getName() {
-		return name;
-	}
-	public char getToken() {
-		return token;
-	}
-	
-	public int getScore() {
-		return score;
-	}
-	
-//	public void addScore
-//	score ++;
+    private String name;
+    private char token;
+    private int score;
+    
+    
+    public Player(String name, char token) {
+        this.name = name;
+        this.token = token;
+    }
+    
+   
+    public String getName() {
+        return name;
+        
+    }
+    
+    
+    public char getToken(){
+            return token;
+    }
+    
+    
+    public int getScore(){
+        return score;
+     }
+    
+    
+    public void addScore(){
+        score++;
+    }
+    
 }
